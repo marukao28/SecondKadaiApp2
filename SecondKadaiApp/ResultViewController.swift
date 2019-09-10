d50fdc02c90edb8887b0c151f0beea9eb993aef3
kaoru.maruyama <marukao28@gmail.com>
2019-09-10 22:15:33 +0900

    中央にオブジェクトを配置

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 丸山薫 on 2019/09/09.
+//  Copyright © 2019 kaoru.maruyama. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var Label: UILabel!
+    // 2画面目のLabelを、StoryboardでこのViewControllerにIBOutletとして接続しておく
+
+    // 受け取るためのプロパティ（変数）を宣言しておく
+    var name:String!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    
+        // 上記では、x,yを0と宣言していたが、
+        // １画面目のViewControllerから遷移するときprepareForSegueで
+        // x,yの値を新たに代入されたので両方とも１が入ってる
+        let result = name
+        Label.text = "結果は \(result!)です"
+    
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textLabel:UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        // 遷移先のResultViewControllerで宣言しているx,yに値を代入してわたす
+        resultViewController.name =  textLabel.text
     }
 
+    @IBAction func unwind(_ segue:UIStoryboardSegue){
+        // 他の画面からsegueを使って戻ってきた時に呼ばれる
+        
+    }
 
 }
 
